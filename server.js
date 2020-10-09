5359e9ad21d3f2204bf1542c74344de5b6c79237
Red Fairy <agorelik2@gmail.com>
2020-10-09 12:41:59 -0400

    html routes

--- server.js
+++ server.js
@@ -5,19 +5,25 @@ const morgan = require("morgan");
 
 // Setting up Express App
 const PORT = process.env.PORT || 4000;
+
+//Setting up database
+const db = require("./models");
+
 const app = express();
+
+//app.use(morgan("tiny"));
+app.use(morgan("dev")); //log to console on development
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useFindAndModify: false, //not sure
 });
-
-//const db = require("./models");
 
 // Creating routes
 require("./routes/api-routes.js")(app);

4193dbb582c748686cf67237842feddf21916db9
Red Fairy <agorelik2@gmail.com>
2020-10-09 13:09:15 -0400

    added route to create a new workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,15 +5,26 @@ module.exports = function (app) {
     // Used by api.js in getLastWorkout()
     app.get("/api/workouts", (req, res) => {
       db.Workout.find({})
-        .then((workout) => {
-          res.json(workout);
+        .then((dbWorkout) => {
+          res.json(dbWorkout);
         })
         .catch((err) => {
           res.json(err);
         });
     });
 
-
+    //Used by api.js in createWorkout({})
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({})
+          .then((dbWorkout) => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+          })
+          .catch((err) => {
+            res.json(err);
+          });
+      });
+    
 
 
 
